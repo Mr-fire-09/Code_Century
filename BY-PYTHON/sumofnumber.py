9a5693efbd4c1bbc76413e0c2931d05bdd8cd719
Neeraj Verma <neerajsingh009424@gmail.com>
2025-01-06 13:04:45 +0530

    sum of first n natural number code was added .

---
+++ BY-PYTHON/sumofnumber.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a Number :"))
+sum = 0
+
+for i in range (num + 1):
+    sum += i
+    
+print ("Sum of first N Natural Number  IS  " , sum)
---
+++ JAVA/sumofnumbers.java
@@ -0,0 +1,27 @@
+package JAVA;
+
+import java.util.Scanner ;
+
+public class sumofnumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter  a  Number : ");
+        int num = sc.nextInt();
+
+
+        int sum = 0;
+
+        for(int i = 0; i <= num; i++){
+            sum = sum + i;
+
+        }
+
+        System.out.println("Sum of Number is :  " + sum );
+
+
+
+
+    }
+}
