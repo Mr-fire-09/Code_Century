95f9dba394a9ea02c41b6bda9d24a3e338d7ee02
Neeraj Verma <neerajsingh009424@gmail.com>
2025-01-09 23:13:33 +0530

    Greatest Of Three Number  Code Updated .

---
+++ BY-PYTHON/greatestofthreenumber.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter Second NUmber : "))
+num3 = int(input("Enter Third NUmber : "))
+
+if num1 > num2 and num1 > num3:
+    print("Number 1 is greater the all numbers")
+    
+elif num2 > num3 and num2 > num3:
+    print("NUmber 2 is greater then all numbers")
+else:
+    print("Number Three is  greater then all numbers")
---
+++ JAVA/greayesyofthreenum.java
@@ -0,0 +1,29 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class greayesyofthreenum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first Number :");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second Number :");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter Third Number :");
+        int num3 = sc.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("Number first in greater the all numbers  ");
+
+        } else if (num2 > num3 && num2 > num3) {
+            System.out.println("Second Number is greater then all numbers");
+
+        } else {
+            System.out.println("NUmber three is greater the all numbers ");
+        }
+    }
+}
